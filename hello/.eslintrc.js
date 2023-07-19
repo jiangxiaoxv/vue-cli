e98fbdc7918e8c25e5c4163e67399c791e6e042b
jiangxiaoxv <1043479536@qq.com>
2023-07-19 21:26:59 +0800

    feat: vue-cli学习初始化

--- hello/.eslintrc.js
+++ hello/.eslintrc.js
@@ -3,25 +3,30 @@
 module.exports = {
   root: true,
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
   },
+  // 告诉eslint代码运行环境，像document，未声明会报错的；env里面的全局变量不会报错
   env: {
-    browser: true
+    browser: true,
+    commonjs: true, // __dirname 未声明不会报错
+    node: true
+    // es2021: true
   },
   extends: [
     // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
     // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
-    "plugin:vue/essential",
-    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
-    "standard"
+    'plugin:vue/essential',
+    'eslint:recommended'
   ],
   // required to lint *.vue files
-  plugins: ["vue"],
+  plugins: ['vue'],
   // add your custom rules here
   rules: {
+    'no-unused-vars': 'warn',
+    quotes: ['error', 'single'],
     // allow async-await
-    "generator-star-spacing": "off",
+    'generator-star-spacing': 'off',
     // allow debugger during development
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   }
-};
+}
